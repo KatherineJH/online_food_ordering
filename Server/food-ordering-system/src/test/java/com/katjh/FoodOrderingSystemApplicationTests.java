957fb877fc5c874416b587d433be57d513b1b436
KatherineJH <ohhomoya@gmail.com>
2025-03-21 11:44:56 +0900

    first commit
    first commit

---
+++ Server/food-ordering-system/src/test/java/com/katjh/FoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
