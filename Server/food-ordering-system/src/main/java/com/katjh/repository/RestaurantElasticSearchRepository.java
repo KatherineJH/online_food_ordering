bf9f0c2dff673ef0e20eb6ab07b3a3a71220ebd5
KatherineJH <ohhomoya@gmail.com>
2025-05-07 14:17:20 +0900

    Update RestaurantElasticSearchRepository.java

--- Server/food-ordering-system/src/main/java/com/katjh/repository/RestaurantElasticSearchRepository.java
+++ Server/food-ordering-system/src/main/java/com/katjh/repository/RestaurantElasticSearchRepository.java
@@ -1,8 +1,8 @@
-package com.katjh.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.katjh.model.RestaurantDocument;
-
-public interface RestaurantElasticSearchRepository
-        extends ElasticsearchRepository<RestaurantDocument, Long> {}
+//package com.katjh.repository;
+//
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//
+//import com.katjh.model.RestaurantDocument;
+//
+//public interface RestaurantElasticSearchRepository
+//        extends ElasticsearchRepository<RestaurantDocument, Long> {}
